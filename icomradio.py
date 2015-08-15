4f4eb9bd764bc32dabdd4a9f98a9742048784a6a
Colin McCabe <mccabe.colin.j@gmail.com>
2015-08-15 17:29:36 -0400

    x

--- icomradio.py
+++ icomradio.py
@@ -50,9 +50,9 @@ class IcomRadio:
     def flush(self):
         self.s.flushInput()
 
-    def cmd(self, cmd_bytes, payload=b'', read_cmd=False):
+    def cmd(self, cmd_bytes, payload=b'', is_read_cmd=False):
         validate = b'\xFE\xFE' + self.comp_addr + self.radio_addr
-        if read_cmd:
+        if is_read_cmd:
             validate += cmd_bytes
 
         to_send = b'\xFE\xFE' + self.radio_addr + self.comp_addr + cmd_bytes + payload + b'\xFD'
@@ -80,7 +80,7 @@ class IcomRadio:
                 else:
                     if response == b'\xFA':
                         raise CivError(response)
-                    if read_cmd:
+                    if is_read_cmd:
                         return response
                     if response != b'\xFB':
                         raise CivError('Unexpected radio response ' + str(response))
@@ -98,7 +98,7 @@ class IcomRadio:
         self.cmd(b'\x16\x12', b)
 
     def read_agc(self):
-        resp = self.cmd(b'\x16\x12', read_cmd=True)
+        resp = self.cmd(b'\x16\x12', is_read_cmd=True)
         if resp == b'\x01':
             return 'Fast'
         elif resp == b'\x02':
@@ -106,29 +106,33 @@ class IcomRadio:
         raise CivError('Unexpected response to AGC read: ' + str(resp))
 
     def set_freq(self, freq):
-        if not freq.isdigit() or len(freq) > 10:
-            raise BadInputError('bad freq')
-        freq = freq.zfill(10)
-        payload = []
-        for x in range(0, 5):
-            payload.insert(0, int(freq[2 * x:2 * x + 2], 16))
+        freq = round(freq)
+        if freq < 0 or freq > 9999999999:
+            raise BadInputError('Freq out of range')
+        payload = b''
+        for n in range(5):
+            digits = (freq // (100**n)) % 100
+            bcd = (((digits // 10) % 10) << 4) | (digits % 10)
+            payload += bytes([bcd])
         self.cmd([0x05], payload)
 
     def read_freq(self):
-        resp = self.cmd([0x03], read_cmd=True)
-        f = resp[8:10] + resp[6:8] + resp[4:6]
-        f += resp[2:4] + resp[0:2]
-        return int(f.lstrip('0'))
+        resp = self.cmd([0x03], is_read_cmd=True)
+        f = 0
+        for n in range(5):
+            f += (((resp[n] >> 4) & 0xF) * 10 + (resp[n] & 0xF)) * 100**n
+        return f
 
     def set_mem(self, mem_ch):
+        mem_ch = round(mem_ch)
         if mem_ch < 2 or mem_ch > 99:
             raise BadInputError('Mem ch out of range: ' + str(mem_ch))
-        bcd = (mem_ch // 10) % 10 << 4 | mem_ch % 10
+        bcd = (((mem_ch // 10) % 10) << 4) | (mem_ch % 10)
         self.cmd(b'\x08')
         self.cmd(b'\x08', bytes([bcd]))
 
     def read_meter(self):
-        resp = self.cmd(b'\x15\x02', read_cmd=True)
+        resp = self.cmd(b'\x15\x02', is_read_cmd=True)
         return ((resp[0] & 0xF) * 100) + ((resp[1] >> 4) & 0xF) * 10 + (resp[1] & 0xF)
 
     def set_mode(self, m):
@@ -138,7 +142,7 @@ class IcomRadio:
         self.cmd(b'\x06', self.BYTES_FOR_MODE[m])
 
     def read_mode(self):
-        resp = self.cmd(b'\x04', read_cmd=True)
+        resp = self.cmd(b'\x04', is_read_cmd=True)
         return self.MODE_FOR_BYTES[resp]
 
     def set_patt(self, patt):
@@ -153,9 +157,9 @@ class IcomRadio:
             raise BadInputError('PAtt must be "Pre", "Att" or "Off"')
 
     def read_patt(self):
-        if self.cmd(b'\x16\x02', read_cmd=True) == b'\x01':
+        if self.cmd(b'\x16\x02', is_read_cmd=True) == b'\x01':
             return 'Pre'
-        if self.cmd(b'\x11', read_cmd=True) == b'\x20':
+        if self.cmd(b'\x11', is_read_cmd=True) == b'\x20':
             return 'Att'
         return 'Off'
 
